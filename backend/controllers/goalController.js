6a408c8c778eeeeb23c065d575c472c7976779aa
sagar442-debug <bnradlyp1@gmail.com>
2023-12-26 01:28:31 -0700

    Added routes and their controllers

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,40 @@
+
+// @desc Get goals
+// @route GET /api/goals
+// @access Private
+const getGoals = (req,res) =>{
+    res.status(200).json({ message: 'Get goals'})
+}
+
+
+// @desc Set goals
+// @route POST /api/goals
+// @access Private
+const setGoal = (req,res) =>{
+    res.status(200).json({ message: 'Set goals'})
+}
+
+
+// @desc Update goals
+// @route Put /api/goals/:id
+// @access Private
+const updateGoal = (req,res) =>{
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+}
+
+
+// @desc Delete goals
+// @route Delte /api/goals/:id
+// @access Private
+const deleteGoal = (req,res) =>{
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+
+
+}
--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,8 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
 app.listen(port, () => console.log(`Server started on ${port}`))
 
