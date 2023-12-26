1a86a5c147ced5ca272f26e7b84ec18c13404dec
sagar442-debug <bnradlyp1@gmail.com>
2023-12-26 01:32:17 -0700

    Cleaning up the route file

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController.js')
 
-router.get('/', getGoals)
+router.route('/').get(getGoals).post(setGoal)
+// Replacing the following code to keep the code clean
+// router.get('/', getGoals)
+// router.post('/', setGoal)
 
-router.post('/', setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
+// Replacing the following code to keep the code clean
+// router.put('/:id', updateGoal)
+// router.delete('/:id', deleteGoal)
 
 
 
