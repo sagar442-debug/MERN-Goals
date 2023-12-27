bee572843b66a0a1f1403f321227069f8975cca3
sagar442-debug <bnradlyp1@gmail.com>
2023-12-26 22:32:19 -0700

    Added asyncHandler to handle error without using try and catch

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,37 +1,38 @@
-
+const asyncHandler = require('express-async-handler')
+const { errorHandler } = require('../middleware/errorMiddleware')
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req,res) =>{
-    if(!req.body.text){
-        res.status(400)
-        throw new Error("Please add text")
-    }
-}
+const getGoals = asyncHandler(async (req,res) =>{
+    res.status(200).json({message: 'Get goals'})
+})
 
 
 // @desc Set goals
 // @route POST /api/goals
 // @access Private
-const setGoal = (req,res) =>{
-    res.status(200).json({ message: 'Set goals'})
-}
+const setGoal = asyncHandler(async (req,res) =>{
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+      }
+})
 
 
 // @desc Update goals
 // @route Put /api/goals/:id
 // @access Private
-const updateGoal = (req,res) =>{
+const updateGoal = asyncHandler(async (req,res) =>{
     res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+})
 
 
 // @desc Delete goals
 // @route Delte /api/goals/:id
 // @access Private
-const deleteGoal = (req,res) =>{
+const deleteGoal = asyncHandler(async (req,res) =>{
     res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
