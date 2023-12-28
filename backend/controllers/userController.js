a328d758c870e347ac49318dda29874538496e03
sagar442-debug <bnradlyp1@gmail.com>
2023-12-27 23:58:53 -0700

    (Revision Required) Adding a middleware to find is the user is authorized or not

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,8 @@ const registerUser = asyncHandler(async (req,res) =>{
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     }else{
         res.status(400)
@@ -61,6 +62,7 @@ const loginUser = asyncHandler(async (req,res) =>{
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id)
         })
     }else{
         res.status(400)
@@ -74,11 +76,17 @@ const loginUser = asyncHandler(async (req,res) =>{
 
 // @desc Get user data
 // @route Get /api/users/me
-// @access Public
+// @access Pivate
 const getMe = asyncHandler(async (req,res) =>{
     res.json({message: 'User data display'})
 })
 
+// Generate JWT 
+const generateToken = (id) =>{
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 
 
