959a1555a7f8a62df6237b8f8e8efece76ecf623
sagar442-debug <bnradlyp1@gmail.com>
2023-12-30 00:18:46 -0700

    Creating header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import {
 import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Header from "./components/Header";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <>
       <Router>
         <div className="container">
+          <Header/>
           <Routes>
             <Route path='/' element={<Dashboard/>}/>
             <Route path='/login' element={<Login/>}/>
