cf113dd50da64262d47b2867b7f4f2fed6d469ba
sagar442-debug <bnradlyp1@gmail.com>
2024-01-03 20:07:01 -0700

    Created goal reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../feature/auth/authSlice';
+import goalReducer from '../feature/goals/goalSlice'
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    goals: goalReducer
   },
 });
