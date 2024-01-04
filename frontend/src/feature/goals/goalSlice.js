fe0010ee6f9b98be21d39621f3454dc6810a4085
sagar442-debug <bnradlyp1@gmail.com>
2024-01-03 20:30:46 -0700

    Donot know what I am doing

--- frontend/src/feature/goals/goalSlice.js
+++ frontend/src/feature/goals/goalSlice.js
@@ -1,5 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-
+import goalService from './goalService'
 
 const initialState = {
     goals: [],
@@ -20,12 +20,52 @@ export const createGoal = createAsyncThunk('goals/create', async(goalData, thunk
     }
 })
 
+// Get user goals
+export const getGoals = createAsyncThunk('goals/getAll', async(_, thunkAPI)=>{
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalService.getGoals(token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message || error.message || error.toString())
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 
 export const goalSlice = createSlice({
     name: 'goal',
     initialState,
     reducers: {
         reset: (state) => initialState
+    },
+    extraReducers: (builder) =>{
+        builder
+        .addCase(createGoal.pending, (state) =>{
+            state.isLoading = true
+        })
+        .addCase(createGoal.fulfilled, (state, action) =>{
+            state.isLoading = false
+            state.isSuccess = true
+            state.goals.push(action.payload)
+        })
+        .addCase(createGoal.rejected, (state, action) =>{
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })
+        .addCase(getGoals.pending, (state) =>{
+            state.isLoading = true
+        })
+        .addCase(getGoals.fulfilled, (state, action) =>{
+            state.isLoading = false
+            state.isSuccess = true
+            state.goals.push(action.payload)
+        })
+        .addCase(getGoals.rejected, (state, action) =>{
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })
     }
 
 })
