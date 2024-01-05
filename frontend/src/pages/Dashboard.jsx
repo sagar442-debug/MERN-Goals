f3552b71ceb794e76c9e972203da165d8696fa81
sagar442-debug <bnradlyp1@gmail.com>
2024-01-04 22:36:31 -0700

    Fixing consistent loop with useeffect

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,16 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import GoalForm from '../components/GoalForm'
-import GoalItem from '../components/GoalItem'
+import {useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
+import GoalForm from '../components/GoalForm';
 import Spinner from '../components/Spinner'
-import { getGoals, reset } from '../features/goals/goalSlice'
+import {getGoals, reset} from '../feature/goals/goalSlice'
+import GoalItem from '../components/GoalItem';
 
 function Dashboard() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-
-  const { user } = useSelector((state) => state.auth)
-  const { goals, isLoading, isError, message } = useSelector(
-    (state) => state.goals
-  )
+  const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
 
   useEffect(() => {
     if (isError) {
@@ -31,30 +28,24 @@ function Dashboard() {
     }
   }, [user, navigate, isError, message, dispatch])
 
-  if (isLoading) {
-    return <Spinner />
+  if(isLoading){
+    return <Spinner/>
   }
 
   return (
     <>
-      <section className='heading'>
-        <h1>Welcome {user && user.name}</h1>
-        <p>Goals Dashboard</p>
-      </section>
-
-      <GoalForm />
-
-      <section className='content'>
-        {goals.length > 0 ? (
-          <div className='goals'>
-            {goals.map((goal) => (
-              <GoalItem key={goal._id} goal={goal} />
-            ))}
-          </div>
-        ) : (
-          <h3>You have not set any goals</h3>
-        )}
-      </section>
+    <section className="heading">
+      <h1>Welcome! {user && user.name}</h1>
+      <p>Goals Dashboard</p>
+    </section>
+    <GoalForm/>
+    <section className="content">
+      {goals.length > 0 ? (
+        <div className="goals">
+          {goals.map((goal) => (<GoalItem key={goal._id} goal={goal}/>))}
+        </div>
+      ): (<h3>You have not set any goals</h3>)}
+    </section>
     </>
   )
 }
