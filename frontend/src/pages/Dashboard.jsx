78d71d58738bcd57af5a8577a92d408c4957b749
sagar442-debug <bnradlyp1@gmail.com>
2024-01-14 12:59:12 -0700

    Fixing error

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,53 +1,67 @@
-import {useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import GoalForm from '../components/GoalForm';
-import Spinner from '../components/Spinner'
-import {getGoals, reset} from '../feature/goals/goalSlice'
+import Spinner from '../components/Spinner';
+import { getGoals, reset } from '../feature/goals/goalSlice';
 import GoalItem from '../components/GoalItem';
 
 function Dashboard() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const {user} = useSelector((state) => state.auth)
-  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector((state) => state.goals);
 
   useEffect(() => {
-    if (isError) {
-      console.log(message)
-    }
+    const fetchData = async () => {
+      try {
+        if (isError) {
+          console.log(message);
+        }
 
-    if (!user) {
-      navigate('/login')
-    }
+        if (!user) {
+          navigate('/login');
+        }
 
-    dispatch(getGoals())
+        await dispatch(getGoals());
+
+        // The following code will execute after goals are fetched
+      } catch (error) {
+        console.error('Error fetching goals:', error);
+      }
+    };
+
+    fetchData();
 
     return () => {
-      dispatch(reset())
-    }
-  }, [user,isError, message, dispatch])
+      dispatch(reset());
+    };
+  }, [user, isError, message, dispatch, navigate]);
 
-  if(isLoading){
-    return <Spinner/>
+  if (isLoading) {
+    return <Spinner />;
   }
 
   return (
     <>
-    <section className="heading">
-      <h1>Welcome! {user && user.name}</h1>
-      <p>Goals Dashboard</p>
-    </section>
-    <GoalForm/>
-    <section className="content">
-      {goals.length > 0 ? (
-        <div className="goals">
-          {goals.map((goal) => (<GoalItem key={goal._id} goal={goal}/>))}
-        </div>
-      ): (<h3>You have not set any goals</h3>)}
-    </section>
+      <section className="heading">
+        <h1>Welcome! {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
